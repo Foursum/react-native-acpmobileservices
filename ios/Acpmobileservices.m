997d60086f95abac08bed8c0a9c3f20636b92171
Adam MacDonald <adam@adammacdonald.ca>
2019-11-01 10:24:01 -0300

    Initial Commit

---
+++ ios/Acpmobileservices.m
@@ -0,0 +1,14 @@
+#import "Acpmobileservices.h"
+
+
+@implementation Acpmobileservices
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+{
+    // TODO: Implement some actually useful functionality
+	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}
+
+@end

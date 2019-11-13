ba23c5d94251eb1ed4d8dd1af6c04a379b0ca047
Adam MacDonald <adam@adammacdonald.ca>
2019-11-13 16:29:59 -0400

    making changes to exports so that module gets picked up properly

--- ios/RCTACPMobileServices.m
+++ ios/RCTACPMobileServices.m
@@ -4,7 +4,12 @@
 
 @implementation RCTACPMobileServices
 
-RCT_EXPORT_MODULE(ACPMobileServices);
+RCT_EXPORT_MODULE();
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
 
 RCT_EXPORT_METHOD(extensionVersion: (RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock) reject) {
     resolve([ACPMobileServices extensionVersion]);
--- js/ACPMobileServices.js
+++ js/ACPMobileServices.js
@@ -2,30 +2,30 @@ import { NativeModules } from 'react-native';
 
 const RCTACPMobileServices = NativeModules.ACPMobileServices;
 
-/**
- * Returns the version of ACPMobileServices extension
- */
-const extensionVersion = () => {
-  return Promise.resolve(RCTACPMobileServices.extensionVersion());
-}
+export default class ACPMobileServices {
+  /**
+   * Returns the version of ACPMobileServices extension
+   */
+  static extensionVersion = () => {
+    return Promise.resolve(RCTACPMobileServices.extensionVersion());
+  }
 
-/**
- * Registers the ACPMobileServices extension with ACPCore
- */
-const registerExtension = () => {
-  RCTACPMobileServices.registerExtension();
-}
+  /**
+   * Registers the ACPMobileServices extension with ACPCore
+   */
+  static registerExtension = () => {
+    RCTACPMobileServices.registerExtension();
+  }
 
-const trackDeepLink = deepLinkURL => {
-  if (deepLinkURL !== undefined && deepLinkURL !== null && deepLinkURL !== "")
-    try {
-      RCTACPMobileServices.trackDeepLink(deepLinkURL)
-    }
-    catch(err) { 
-      throw "Deep link could not be tracked or was invalid!";
-    }
-  else
-    throw "Deep link URL is required!";
+  static trackDeepLink = deepLinkURL => {
+    if (deepLinkURL !== undefined && deepLinkURL !== null && deepLinkURL !== "")
+      try {
+        RCTACPMobileServices.trackDeepLink(deepLinkURL)
+      }
+      catch(err) { 
+        throw "Deep link could not be tracked or was invalid!";
+      }
+    else
+      throw "Deep link URL is required!";
+  }
 }
-
-export { extensionVersion, registerExtension }
